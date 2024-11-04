3ec5f76fa91dd34ade5c5faa3b4e8ccdb1f39a80
Sourabh_Sawant <124140322+Sourabh1858@users.noreply.github.com>
2024-11-04 18:02:34 +0530

    modified ui of sidebar and navbar

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
-from .models import NewUser,Project
+from .models import NewUser
 
 class LoginForm(forms.Form):
     fields = ['username', 'password']
